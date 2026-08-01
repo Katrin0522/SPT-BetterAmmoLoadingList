99c5ca7510f4b3fd33eb00e1f73284a9d206b721
Katrin0522 <katborowsky@gmail.com>
2026-08-01 22:18:28 +0300

    increase ver

--- BetterAmmoLoadingListPlugin.cs
+++ BetterAmmoLoadingListPlugin.cs
@@ -5,7 +5,7 @@ using BetterAmmoLoadingList.Patch;
 
 namespace BetterAmmoLoadingList
 {
-    [BepInPlugin("katrin0522.betterammoloadinglist", "BALL - BetterAmmoLoadingList", "1.1.0")]
+    [BepInPlugin("katrin0522.betterammoloadinglist", "BALL - BetterAmmoLoadingList", "1.1.1")]
     public class BetterAmmoLoadingListPlugin : BaseUnityPlugin
     {
         public static BetterAmmoLoadingListPlugin Instance { get; private set; }
